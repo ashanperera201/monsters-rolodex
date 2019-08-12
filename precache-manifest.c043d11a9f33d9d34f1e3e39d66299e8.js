33fb336bf7013b9b8842816e33f730114a3d2150
Ashan Perera <ashanperera.201@gmail.com>
2019-08-12 19:02:50 +0530

    Updates

---
+++ precache-manifest.c043d11a9f33d9d34f1e3e39d66299e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58dfe8458af353ae0926b0e1b0b18f3d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "459e56ee5c119b75e98d",
+    "url": "/monsters-rolodex/static/css/main.2c56d8cc.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "459e56ee5c119b75e98d",
+    "url": "/monsters-rolodex/static/js/main.0b90e6d5.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
